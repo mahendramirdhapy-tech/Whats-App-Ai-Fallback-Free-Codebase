32fab5476fc3f959501a3ddfda0a32000e99b11c
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-26 17:10:37 +0530

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,55 @@
+class Logger {
+  constructor() {
+    this.logLevel = process.env.LOG_LEVEL || 'info';
+    this.levels = {
+      error: 0,
+      warn: 1,
+      info: 2,
+      debug: 3
+    };
+  }
+
+  shouldLog(level) {
+    return this.levels[level] <= this.levels[this.logLevel];
+  }
+
+  formatMessage(level, message, meta = {}) {
+    const timestamp = new Date().toISOString();
+    const logEntry = {
+      timestamp,
+      level: level.toUpperCase(),
+      message,
+      ...meta
+    };
+
+    return JSON.stringify(logEntry);
+  }
+
+  log(level, message, meta = {}) {
+    if (this.shouldLog(level)) {
+      console.log(this.formatMessage(level, message, meta));
+    }
+  }
+
+  error(message, meta = {}) {
+    this.log('error', message, meta);
+  }
+
+  warn(message, meta = {}) {
+    this.log('warn', message, meta);
+  }
+
+  info(message, meta = {}) {
+    this.log('info', message, meta);
+  }
+
+  debug(message, meta = {}) {
+    this.log('debug', message, meta);
+  }
+}
+
+const logger = new Logger();
+
+module.exports = {
+  logger
+};
